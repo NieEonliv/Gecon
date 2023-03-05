dbf7bcbb4878f6caa9fe57d0a469129f821aa6e4
root <nieeonliv@gmail.com>
2023-03-05 13:58:55 +0300

    fix links

--- src/resources/views/hablons/curseinfo.blade.php
+++ src/resources/views/hablons/curseinfo.blade.php
@@ -23,37 +23,43 @@
                 </div>
             </div>
             <div class="col-md-2 ms-auto">
-                @foreach(\Illuminate\Support\Facades\Auth::user()->course as $enroll)
-                    @if($enroll->course->id == $course->id )
-                        @php($bool = true)
-                        <form action="{{ route('course.show', $course->id) }}" method="get">
-                            <button class="btn btn-primary w-100">Перейти к курсу</button>
-                        </form>
-                        @break
-                    @endif
-                @endforeach
-                @if(!isset($bool))
-                    <form action="{{ route('course.enroll', $course->id) }}" method="post">
-                        @csrf
-                        <button class="btn btn-primary w-100">Поступить на курс</button>
+                @guest()
+                    <form action="{{ route('login') }}" method="get">
+                        <button class="btn btn-primary w-100">Перейти к курсу</button>
                     </form>
-                @endif
+                @else
+                    @foreach(\Illuminate\Support\Facades\Auth::user()->course as $enroll)
+                        @if($enroll->course->id == $course->id )
+                            @php($bool = true)
+                            <form action="{{ route('course.show', $course->id) }}" method="get">
+                                <button class="btn btn-primary w-100">Перейти к курсу</button>
+                            </form>
+                            @break
+                        @endif
+                    @endforeach
+                    @if(!isset($bool))
+                        <form action="{{ route('course.enroll', $course->id) }}" method="post">
+                            @csrf
+                            <button class="btn btn-primary w-100">Поступить на курс</button>
+                        </form>
+                    @endif
+                @endguest
                 <div class="mt-3 p-2" style="border: 3px solid green; border-radius: 10px">
                     <h4>В курс входят:</h4>
-                    <?php
-                    $lecture = 0;
-                    $practical = 0;
-                    foreach ($course->module as $relation) {
-                        $occupations = $relation->module->occupation;
-                        foreach ($occupations as $occupation) {
-                            if ($occupation->occupation->type === 'lecture') {
-                                $lecture++;
-                            } else {
-                                $practical++;
+                        <?php
+                        $lecture = 0;
+                        $practical = 0;
+                        foreach ($course->module as $relation) {
+                            $occupations = $relation->module->occupation;
+                            foreach ($occupations as $occupation) {
+                                if ($occupation->occupation->type === 'lecture') {
+                                    $lecture++;
+                                } else {
+                                    $practical++;
+                                }
                             }
                         }
-                    }
-                    ?>
+                        ?>
                     <p>{{ $lecture }} уроков</p>
                     <p>{{ $practical }} заданий</p>
                 </div>
